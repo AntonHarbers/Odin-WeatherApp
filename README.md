86114deba69f069117d15e85f26a94a59ad7f970
Anton Harbers <Anton.Harbers23@gmail.com>
2023-08-03 10:44:18 +0400

    Added basic 3 day forecast

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ A basic weather app for the odin project.
 Todos:
 
 // show forecast for next 3 days below main view
-// show forecast for next 12 hours with slider? (change background color depending on where slider is?)
 
 // if cloudy, add animated clouds, more cloudy more clouds
 // if rain, add animated rain
--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const endpoint = "http://api.weatherapi.com/v1";
 const apiKey = "6c5d19286ef047d387d103759230108";
 let city = "London";
 
+let forecastEndpoint = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=3&aqi=no&alerts=no`
+
 // main info elements
 const conditionImg = document.querySelector("#conditionImg");
 const cityText = document.querySelector("#city");
@@ -18,9 +20,11 @@ const humidityText = document.querySelector("#humidity");
 const uvText = document.querySelector("#uv");
 const pressureText = document.querySelector("#pressure");
 
+
 let isCelcius = true;
 let isDay = true;
 let isWindKph = true;
+
 const celciusCheckbox = document.querySelector("#checkbox");
 const celciusCheckboxLabel = document.querySelector("#checkboxLabel");
 const windCheckbox = document.querySelector("#windCheckbox");
@@ -28,9 +32,49 @@ const windCheckboxLabel = document.querySelector("#windCheckboxLabel");
 
 const searchForm = document.querySelector("form");
 
+const forecastDay1Date = document.querySelector("#forecastDay1Date");
+const forecastDay1ConditionImg = document.querySelector("#forecastDay1Img");
+const forecastDay1Temp = document.querySelector("#forecastDay1Temp");
+
+const forecastDay2Date = document.querySelector("#forecastDay2Date");
+const forecastDay2ConditionImg = document.querySelector("#forecastDay2Img");
+const forecastDay2Temp = document.querySelector("#forecastDay2Temp");
+
+const forecastDay3Date = document.querySelector("#forecastDay3Date");
+const forecastDay3ConditionImg = document.querySelector("#forecastDay3Img");
+const forecastDay3Temp = document.querySelector("#forecastDay3Temp");
+
+
+// load isCelcius and isWindKph from local storage
+if (localStorage.getItem("isCelcius") === null) {
+  localStorage.setItem("isCelcius", true);
+} else {
+  isCelcius = JSON.parse(localStorage.getItem("isCelcius"));
+  if (isCelcius) {
+    celciusCheckboxLabel.innerHTML = "°C";
+  } else {
+    celciusCheckboxLabel.innerHTML = "°F";
+    celciusCheckbox.checked = true;
+  }
+}
+
+if (localStorage.getItem("isWindKph") === null) {
+  localStorage.setItem("isWindKph", true);
+} else {
+  isWindKph = JSON.parse(localStorage.getItem("isWindKph"));
+  if (isWindKph) {
+    windCheckboxLabel.innerHTML = "km/h";
+  } else {
+    windCheckboxLabel.innerHTML = "mph";
+    windCheckbox.checked = true;
+  }
+}
+
+
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   city = e.target[0].value;
+  forecastEndpoint = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=3&aqi=no&alerts=no`
 
   getWeather();
 });
@@ -42,6 +86,9 @@ celciusCheckbox.addEventListener("change", () => {
   } else {
     celciusCheckboxLabel.innerHTML = "°F";
   }
+  // set local storage 
+  localStorage.setItem("isCelcius", isCelcius);
+
   getWeather();
 });
 
@@ -52,6 +99,9 @@ windCheckbox.addEventListener("change", () => {
   } else {
     windCheckboxLabel.innerHTML = "mph";
   }
+
+  // set local storage
+  localStorage.setItem("isWindKph", isWindKph);
   getWeather();
 });
 
@@ -62,9 +112,7 @@ const getWeather = async () => {
   document.body.appendChild(spinner);
 
   try {
-    const response = await fetch(
-      `${endpoint}/current.json?key=${apiKey}&q=${city}`
-    );
+    const response = await fetch(forecastEndpoint);
     const data = await response.json();
     console.log(data);
 
@@ -87,6 +135,22 @@ const getWeather = async () => {
     uvText.innerHTML = await data.current.uv;
     pressureText.innerHTML = (await data.current.pressure_mb) + " mb";
 
+    forecastDay1Date.textContent = await data.forecast.forecastday[0].date;
+    forecastDay1ConditionImg.src = await data.forecast.forecastday[0].day.condition.icon;
+    forecastDay2Date.textContent = await data.forecast.forecastday[1].date;
+    forecastDay2ConditionImg.src = await data.forecast.forecastday[1].day.condition.icon;
+    forecastDay3Date.textContent = await data.forecast.forecastday[2].date;
+    forecastDay3ConditionImg.src = await data.forecast.forecastday[2].day.condition.icon;
+    
+    if (isCelcius) {
+      forecastDay1Temp.textContent = await data.forecast.forecastday[0].day.avgtemp_c + "°C";
+      forecastDay2Temp.textContent = await data.forecast.forecastday[1].day.avgtemp_c + "°C";
+      forecastDay3Temp.textContent = await data.forecast.forecastday[2].day.avgtemp_c + "°C";
+    } else {
+      forecastDay1Temp.textContent = await data.forecast.forecastday[0].day.avgtemp_f + "°F";
+      forecastDay2Temp.textContent = await data.forecast.forecastday[1].day.avgtemp_f + "°F";
+      forecastDay3Temp.textContent = await data.forecast.forecastday[2].day.avgtemp_f + "°F";
+    }
     
     // set wind direction image according to wind direction
     console.log(await data.current.wind_dir)
@@ -101,11 +165,11 @@ const getWeather = async () => {
       windDirectionImage.src = "images/arrows/left.png";
     } else if (windDirection === "NE") {
       windDirectionImage.src = "images/arrows/up-right.png";
-    } else if (windDirection === "NNW" || windDirection === "WNW") {
+    } else if (windDirection === "NNW" || windDirection === "WNW" || windDirection === "NW") {
       windDirectionImage.src = "images/arrows/up-left.png";
     } else if (windDirection === "SE") {
       windDirectionImage.src = "images/arrows/down-right.png";
-    } else if (windDirection === "WSW" || windDirection === "SSW") {
+    } else if (windDirection === "WSW" || windDirection === "SSW" || windDirection === "SW") {
       windDirectionImage.src = "images/arrows/down-left.png";
     }
 
@@ -118,9 +182,11 @@ const getWeather = async () => {
 
     // set body background color according to day/night
     if (await data.current.is_day) {
-      document.body.style.backgroundColor = "blue";
+      // set background image to a gradient that looks like a sunny day
+      document.body.style.backgroundImage = "linear-gradient(skyblue, orange)";
+
     } else {
-      document.body.style.backgroundColor = "midnightblue";
+      document.body.style.backgroundImage = "linear-gradient(black, midnightblue)";
     }
   } catch (e) {
     console.log(e);
